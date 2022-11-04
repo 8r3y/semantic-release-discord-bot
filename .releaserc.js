ca31916ffe8e5bda096d82fc42f29cc29aade182
Andrey Potapov <info@nsstms.ru>
2022-11-04 13:54:24 +0700

    feat: added semantic release

---
+++ .releaserc.js
@@ -0,0 +1,26 @@
+const ref = process.env.GITHUB_REF;
+const branch = ref.split('/').pop();
+
+function getPlugins() {
+	if (branch === 'master') {
+		return [
+			'@semantic-release/commit-analyzer',
+			'@semantic-release/release-notes-generator',
+			['@semantic-release/changelog', {changelogFile: 'CHANGELOG.md'}],
+			['@semantic-release/npm', {npmPublish: false}],
+			[
+				'@semantic-release/git',
+				{
+					assets: ['CHANGELOG.md', 'package.json'],
+					message: '${nextRelease.version} CHANGELOG [skip ci]\n\n${nextRelease.notes}',
+				},
+			],
+			'@semantic-release/github',
+		];
+	}
+}
+
+module.exports = {
+	branches: ['master'],
+	plugins: getPlugins(),
+};
